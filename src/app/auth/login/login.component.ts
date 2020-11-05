44c6b0adcd3db95c410d7e1b129c375ab8be2a5e
dazelpro <dazelpro@gmail.com>
2020-11-05 10:49:07 +0700

    login auth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,26 +9,14 @@ import { ActivatedRoute } from '@angular/router';
 export class LoginComponent implements OnInit {
 
     auth2: any;
+    dataUser;
     @ViewChild('openDialogGoogle', {static: true }) loginElement: ElementRef;
     
     constructor(private route: ActivatedRoute) { }
 
     ngOnInit(): void {
-        this.route.data.subscribe(v => console.log(v));
+        // this.route.data.subscribe(v => console.log(v));
         this.googleSDK();
-    }
-    prepareLoginButton() {
-        this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
-        (googleUser) => {
-            let profile = googleUser.getBasicProfile();
-            console.log('Token || ' + googleUser.getAuthResponse().id_token);
-            console.log('ID: ' + profile.getId());
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail());
-        }, (error) => {
-            console.log(JSON.stringify(error, undefined, 2));
-        });
     }
 
     googleSDK() {
@@ -36,7 +24,8 @@ export class LoginComponent implements OnInit {
             window['gapi'].load('auth2', () => {
                 this.auth2 = window['gapi'].auth2.init({
                     client_id: '773848106670-2g6b45578b954pcp5avdfkohotf8n95t.apps.googleusercontent.com',
-                    cookiepolicy: 'single_host_origin',
+                    // cookiepolicy: 'single_host_origin',
+                    cookiepolicy: 'https://dazelpro.com/privacy',
                     scope: 'profile email'
                 });
                 this.prepareLoginButton();
@@ -50,6 +39,28 @@ export class LoginComponent implements OnInit {
             js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'google-jssdk'));
+    }
+
+    prepareLoginButton() {
+        this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
+        (googleUser) => {
+            let profile = googleUser.getBasicProfile();
+            // console.log('Token || ' + googleUser.getAuthResponse().id_token);
+            this.dataUser = {
+                userID : profile.getId(),
+                userEmail : profile.getEmail(),
+                userName : profile.getName(),
+                userPhotoUrl : profile.getImageUrl(),
+                userStatus: 0
+            };
+            this.login();
+        }, (error) => {
+            console.log(JSON.stringify(error, undefined, 2));
+        });
+    }
+
+    async login() {
+        console.log(this.dataUser)
     }
 
 }
