7790d68cc08031c04b8066b94599ccf5ec2f7e4c
dazelpro <dazelpro@gmail.com>
2020-11-06 22:44:54 +0700

    desain home

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 const ExportMatModule = [   
     MatButtonModule,
     MatRippleModule,
-    MatDialogModule
+    MatDialogModule,
+    MatIconModule
 ]
 
 @NgModule({
