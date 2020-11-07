20195148fd65ad63814525f750cd52cd085eeb02
dazelpro <dazelpro@gmail.com>
2020-11-07 23:14:04 +0700

    + animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,6 +1,7 @@
 import { trigger, transition, style, query, animateChild, group, animate } from '@angular/animations';
 
-export const routeTransitionAnimations = trigger('triggerName', [
+export const routeTransitionAnimations = 
+trigger('triggerName', [
 	transition('One => Two, Two => Three, One => Three', [
 		style({ position: 'relative' }),
 		query(':enter, :leave', [
@@ -8,14 +9,15 @@ export const routeTransitionAnimations = trigger('triggerName', [
 				position: 'absolute',
 				top: 0,
 				right: 0,
-				width: '100%'
+				width: '100%',
+				height: '0px'
 			})
 		]),
-		query(':enter', [style({ right: '-100%', opacity: 0 })]),
+		query(':enter', [style({ right: '-100%', opacity: 0, overflow: 'hidden' })]),
 		query(':leave', animateChild()),
 		group([
-			query(':leave', [animate('300ms ease-out', style({ right: '100%', opacity: 0 }))]),
-			query(':enter', [animate('300ms ease-out', style({ right: '0%', opacity: 1 }))])
+			query(':leave', [animate('200ms ease-out', style({ right: '100%', opacity: 0, overflow: 'hidden' }))]),
+			query(':enter', [animate('200ms ease-out', style({ right: '0%', opacity: 1, overflow: 'hidden' }))])
 		]),
 		query(':enter', animateChild())
 	]),
@@ -26,15 +28,16 @@ export const routeTransitionAnimations = trigger('triggerName', [
 				position: 'absolute',
 				top: 0,
 				left: 0,
-				width: '100%'
+				width: '100%',
+				height: '0px'
 			})
 		]),
-		query(':enter', [style({ left: '-100%', opacity: 0 })]),
+		query(':enter', [style({ left: '-100%', opacity: 0, overflow: 'hidden' })]),
 		query(':leave', animateChild()),
 		group([
-			query(':leave', [animate('300ms ease-out', style({ left: '100%', opacity: 0 }))]),
-			query(':enter', [animate('300ms ease-out', style({ left: '0%', opacity: 1 }))])
+			query(':leave', [animate('200ms ease-out', style({ left: '100%', opacity: 0, overflow: 'hidden' }))]),
+			query(':enter', [animate('200ms ease-out', style({ left: '0%', opacity: 1, overflow: 'hidden' }))])
 		]),
 		query(':enter', animateChild())
-	])
+	]),
 ]);
