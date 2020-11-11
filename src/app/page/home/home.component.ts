ec0aeac9487ef121b9f754868f42dbff4fd1c30b
dazelpro <dazelpro@gmail.com>
2020-11-11 21:46:09 +0700

    moving bottom

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddTransactionComponent } from '../add-transaction/add-transaction.component';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        public dialog: MatDialog
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    openDialogAdd() {
+        const dialogRef = this.dialog.open(AddTransactionComponent, {
+            width: '400px',
+            data: {},
+        });
+        dialogRef.afterClosed().subscribe(arr => {
+            // this.loading = true;
+            this.ngOnInit();
+        });
+    }
 
 }
