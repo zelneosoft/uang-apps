14e415680832d557cac60d191fb037ea176b6beb
dazelpro <dazelpro@gmail.com>
2020-11-19 14:47:00 +0700

    about

--- src/app/page/setting/main-setting/main-setting.component.ts
+++ src/app/page/setting/main-setting/main-setting.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-main-setting',
-  templateUrl: './main-setting.component.html',
-  styleUrls: ['./main-setting.component.css']
+    selector: 'app-main-setting',
+    templateUrl: './main-setting.component.html',
+    styleUrls: ['./main-setting.component.css']
 })
 export class MainSettingComponent implements OnInit {
 
-  constructor() { }
+    versi;
+    constructor() { 
+        this.versi = environment.versiApp 
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
