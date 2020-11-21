87a4501fa6ac7f8355ce4a9c1be48137ee1685c7
dazelpro <dazelpro@gmail.com>
2020-11-21 19:39:36 +0700

    desain dialog delete

--- src/app/page/cash-out/dialog-confirm-delete-all/dialog-confirm-delete-all.component.ts
+++ src/app/page/cash-out/dialog-confirm-delete-all/dialog-confirm-delete-all.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-dialog-confirm-delete-all',
-  templateUrl: './dialog-confirm-delete-all.component.html',
-  styleUrls: ['./dialog-confirm-delete-all.component.css']
+    selector: 'app-dialog-confirm-delete-all',
+    templateUrl: './dialog-confirm-delete-all.component.html',
+    styleUrls: ['./dialog-confirm-delete-all.component.css']
 })
 export class DialogConfirmDeleteAllComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        public dialogRef: MatDialogRef<DialogConfirmDeleteAllComponent>,
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    delete() {
+        this.dialogRef.close(true);
+    }
 
 }
