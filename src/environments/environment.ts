aa7280b1279d1d11c47f9f0d9a7b547db40fb4b1
dazelpro <dazelpro@gmail.com>
2020-12-16 18:16:05 +0700

    rubah env ke backend online

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  urlApi: 'http://localhost:8000',
+  // urlApi: 'http://localhost:8000',
+  urlApi: 'https://api.dazelpro.com',
   versiApp : '1.0.8 Dev'
 };
