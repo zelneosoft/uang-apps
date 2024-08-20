afedda7c5fe114a560c3ff1cf3ec515c2a6c6ad8
unknown <zeldiantoneosoft@gmail.com>
2024-08-20 20:15:36 +0700

    tes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // urlApi: 'http://localhost:8000',
   urlApi: 'https://api.dazelpro.com',
-  versiApp : '1.0.9 Dev'
+  versiApp : '1.0.9 Dev '
 };
